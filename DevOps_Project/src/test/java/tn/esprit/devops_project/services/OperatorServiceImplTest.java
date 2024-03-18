3c293122dec283a5c5ee069e9e599130ef22eed1
Ahmed Gdoura <ahmedgdoura007@gmail.com>
2024-03-18 08:55:07 +0100

    comit

--- DevOps_Project/src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
@@ -1,5 +1,4 @@
 package tn.esprit.devops_project.services;
-
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,20 +11,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import tn.esprit.devops_project.entities.Operator;
 import tn.esprit.devops_project.repositories.OperatorRepository;
-
 import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 class OperatorTest {
     @Mock
-    private OperatorRepository repositoryMocks;
+    private OperatorRepository repository;
     @InjectMocks
-    private OperatorServiceImpl serviceMocks;
+    private OperatorServiceImpl service;
     @SpringBootTest
     @Nested
     class OperatorServiceImplTest {
@@ -40,36 +36,15 @@ class OperatorTest {
             Mockito.when(operatorRepository.findById(1L)).thenReturn(Optional.of(operator));
 
             // Act
-            Operator retrievedOperator = serviceMocks.retrieveOperator(1L);
+            Operator retrievedOperator = service.retrieveOperator(1L);
 
             // Assert
             assertNotNull(retrievedOperator);
             assertEquals(1L, retrievedOperator.getIdOperateur()); // Improved assertion for better readability
             assertEquals("test", retrievedOperator.getFname());
 
-            Mockito.verify(repositoryMocks, Mockito.times(1)).findById(1L);
+            Mockito.verify(repository, Mockito.times(1)).findById(1L);
 
         }
-    }
-    @Test
-    void addOperator() {
-        // Prepare test data
-        Operator operatorToAdd = new Operator();
-        operatorToAdd.setFname("John");
-        operatorToAdd.setLname("Doe");
-
-        // Mock behavior
-        when(repositoryMocks.save(any(Operator.class))).thenReturn(operatorToAdd);
-
-        // Call the method from the service to add the operator
-        Operator addedOperator = serviceMocks.addOperator(operatorToAdd);
-
-        // Assert the result
-        assertNotNull(addedOperator);
-        assertEquals("Ahmed", addedOperator.getFname());
-        assertEquals("Gdoura", addedOperator.getLname());
-
-        // Verify that the repository method was called with the correct parameter
-        verify(repositoryMocks, times(1)).save(operatorToAdd);
     }
 }
